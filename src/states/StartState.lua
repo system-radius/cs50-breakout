b3697b95ce66cfc0158d19f1e0504b26605a927f
system-radius <radius.sairo@gmail.com>
2020-05-11 20:42:33 +0800

    refactor: allow inclusion of other options in the future easily

--- src/states/StartState.lua
+++ src/states/StartState.lua
@@ -4,26 +4,47 @@ local highlighted = 1
 
 function StartState:enter(params)
   self.highscores = params.highscores
+  
+  self.minHighlight = 1
+  highlighted = self.minHighlight
+  self.optionCount = 3
+  
+  self.nextStates = {
+    [1] = {
+      state = 'paddle-select',
+      params = {
+        highscores = self.highscores
+      },
+      display = 'START',
+      y = VIRTUAL_HEIGHT / 2 + 50
+    },
+    [2] = {
+      state = 'highscore',
+      params = {
+        highscores = self.highscores
+      },
+      display = 'HIGH SCORE',
+      y = VIRTUAL_HEIGHT / 2 + 70
+    }
+  
+  }
 end
 
 function StartState:update(dt)
 
-  if love.keyboard.wasPressed('up') or  love.keyboard.wasPressed('down') then
-    highlighted = highlighted == 1 and 2 or 1
+  if love.keyboard.wasPressed('up') then
+    highlighted = math.max(highlighted - 1, self.minHighlight) 
+    gSounds['paddle-hit']:stop()
+    gSounds['paddle-hit']:play()
+  elseif love.keyboard.wasPressed('down') then
+    highlighted = math.min(highlighted + 1, self.optionCount) 
+    gSounds['paddle-hit']:stop()
     gSounds['paddle-hit']:play()
   end
 
   if love.keyboard.wasPressed('enter') or love.keyboard.wasPressed('return') then
     gSounds['confirm']:play()
-    if highlighted == 1 then
-      gStateMachine:change('paddle-select', {
-        highscores = self.highscores
-      })
-    else
-      gStateMachine:change('highscore', {
-        highscores = self.highscores
-      })
-    end
+    gStateMachine:change(self.nextStates[highlighted].state, self.nextStates[highlighted].params)
   end
 
   if love.keyboard.wasPressed('escape') then
@@ -40,19 +61,17 @@ function StartState:render()
 
   love.graphics.setFont(gFonts['medium'])
 
-  if highlighted == 1 then
-    love.graphics.setColor(103, 255, 255, 255)
-  end
-  love.graphics.printf('START', 0, VIRTUAL_HEIGHT / 2 + 70,
-    VIRTUAL_WIDTH, 'center')
+  for i, option in pairs(self.nextStates) do
 
-  love.graphics.setColor(255, 255, 255, 255)
+    love.graphics.setColor(255, 255, 255, 255)
+    if highlighted == i then
+      love.graphics.setColor(103, 255, 255, 255)
+    end
 
-  if highlighted == 2 then
-    love.graphics.setColor(103, 255, 255, 255)
+    if i >= self.minHighlight then
+      love.graphics.printf(option.display, 0, option.y, VIRTUAL_WIDTH, 'center')
+    end
   end
-  love.graphics.printf('HIGH SCORES', 0, VIRTUAL_HEIGHT / 2 + 90 ,
-    VIRTUAL_WIDTH, 'center')
 
   love.graphics.setColor(255, 255, 255, 255)
 end
